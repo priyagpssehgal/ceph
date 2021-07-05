737868203ad0210a9306c4e79f7f004be5884a83
Priya Sehgal <priya.sehgal@flipkart.com>
2021-07-05 13:03:37 +0530

    Fixed the behavior of PUT on SSE-S3 bucket as supported by AWS S3. If a PUT request comes with AWS:KMS headers on a bucket, which has encryption enabled using SSE-S3, the object is encrypted using AWS:KMS keys and not SSE-S3 keys. This is similar to behavior as seen on AWS S3.
    Signed-off-by: Priya Sehgal <priya.sehgal@flipkart.com>

--- src/rgw/rgw_crypt.cc
+++ src/rgw/rgw_crypt.cc
@@ -908,69 +908,7 @@ int rgw_s3_prepare_encrypt(struct req_state* s,
   ldpp_dout(s, 5) << "PRIYA: Inside prepare_encrypt " << dendl;
 
   {
-    /* Checking bucket attributes if SSE is enabled. Currently only supporting SS#-S3 */
-    rgw::sal::Attrs buck_attrs(s->bucket_attrs);
-    auto aiter = buck_attrs.find(RGW_ATTR_BUCKET_ENCRYPTION); 
-    if (aiter == buck_attrs.end()) {
-      ldpp_dout(s, 5) << "PRIYA: can't find RGW_ATTR_BUCKET_ENCRYPTION attr bucket_name = " << s->bucket_name << dendl;
-    } else {
-      ldpp_dout(s, 5) << "PRIYA: found RGW_ATTR_BUCKET_ENCRYPTION attr bucket_name = " << s->bucket_name << dendl;
-      bufferlist::const_iterator iter{&aiter->second};
-      try {
-        RGWBucketEncryptionConfig bucket_encryption_conf;
-	bucket_encryption_conf.decode(iter);
-	if (bucket_encryption_conf.sse_algorithm() == "AES256") {
-	  ldpp_dout(s, 5) << "PRIYA: ALGO: " <<  bucket_encryption_conf.sse_algorithm() << dendl;
-	  ldpp_dout(s, 5) << "PRIYA: KEY " <<  bucket_encryption_conf.kms_master_key_id() << dendl;
-	  std::string_view context = "";
-          std::string cooked_context;
-	  if ((res = make_canonical_context(s, context, cooked_context)))
-	    return res;
-
-	  //key_id = bucket_encryption_conf.kms_master_key_id();
-	  std::string_view key_id = s->bucket->get_info().owner.id; // TODO - DELETE this
-	  std::string key_selector = create_random_key_selector(s->cct);
-
-	  set_attr(attrs, RGW_ATTR_CRYPT_KEYSEL, key_selector);
-	  set_attr(attrs, RGW_ATTR_CRYPT_CONTEXT, cooked_context);
-	  set_attr(attrs, RGW_ATTR_CRYPT_MODE, "AES256");
-	  set_attr(attrs, RGW_ATTR_CRYPT_KEYID, key_id);
-	  std::string actual_key;
-	  res = make_actual_key_from_sse_s3(s->cct, attrs, actual_key);
-          if (res != 0) {
-	    ldpp_dout(s, 5) << "ERROR: failed to retrieve actual key from key_id: " << key_id << dendl;
-	    s->err.message = "Failed to retrieve the actual key " ;
-	    return res;
-	  }
-	  if (actual_key.size() != AES_256_KEYSIZE) {
-	    ldpp_dout(s, 5) << "ERROR: key obtained from key_id:" <<
-                           key_id << " is not 256 bit size" << dendl;
-	    s->err.message = "KMS provided an invalid key for the given kms-keyid.";
-	    return -ERR_INVALID_ACCESS_KEY;
-	  }
-
-          if (block_crypt) {
-	    auto aes = std::unique_ptr<AES_256_CBC>(new AES_256_CBC(s->cct));
-	    aes->set_key(reinterpret_cast<const uint8_t*>(actual_key.c_str()), AES_256_KEYSIZE);
-	    *block_crypt = std::move(aes);
-	  }
-          ::ceph::crypto::zeroize_for_security(actual_key.data(), actual_key.length());
-
-	  crypt_http_responses["x-amz-server-side-encryption"] = "AES256";
-
-	  return 0;
-	} else {
-          ldpp_dout(s, 5) << "ERROR: SSE algorithm: " 
-		  <<  bucket_encryption_conf.sse_algorithm() 
-		  << " not supported" << dendl;
-	  s->err.message = "The requested encryption algorithm is not valid, must be AES256.";
-	  return -ERR_INVALID_ENCRYPTION_ALGORITHM;
-	}
-      } catch (const buffer::error& e) {
-        ldpp_dout(s, 5) << __func__ <<  "decode bucket_encryption_conf failed" << dendl;
-      }
-    }
-
+    
     std::string_view req_sse_ca =
         get_crypt_attribute(s->info.env, parts, X_AMZ_SERVER_SIDE_ENCRYPTION_CUSTOMER_ALGORITHM);
     if (! req_sse_ca.empty()) {
@@ -1153,6 +1091,71 @@ int rgw_s3_prepare_encrypt(struct req_state* s,
         s->err.message = "Server Side Encryption with KMS managed key requires "
                          "HTTP header x-amz-server-side-encryption : aws:kms";
         return -EINVAL;
+      }
+    }
+
+    /* Checking bucket attributes if SSE is enabled. Currently only supporting SSE-S3 */
+    rgw::sal::Attrs buck_attrs(s->bucket_attrs);
+    auto aiter = buck_attrs.find(RGW_ATTR_BUCKET_ENCRYPTION); 
+    if (aiter != buck_attrs.end()) {
+      ldpp_dout(s, 5) << "Found RGW_ATTR_BUCKET_ENCRYPTION attr bucket_name: " 
+	      << s->bucket_name << dendl;
+
+      bufferlist::const_iterator iter{&aiter->second};
+      try {
+        RGWBucketEncryptionConfig bucket_encryption_conf;
+	bucket_encryption_conf.decode(iter);
+	if (bucket_encryption_conf.sse_algorithm() == "AES256") {
+	  ldpp_dout(s, 5) << "RGW_ATTR_BUCKET_ENCRYPTION ALGO: " 
+		  <<  bucket_encryption_conf.sse_algorithm() << dendl;
+	  ldpp_dout(s, 5) << "RGW_ATTR_BUCKET_ENCRYPTION KEY " 
+		  <<  bucket_encryption_conf.kms_master_key_id() << dendl;
+	  std::string_view context = "";
+          std::string cooked_context;
+	  if ((res = make_canonical_context(s, context, cooked_context)))
+	    return res;
+
+	  //key_id = bucket_encryption_conf.kms_master_key_id();
+	  std::string_view key_id = s->bucket->get_info().owner.id; // TODO - DELETE this
+	  std::string key_selector = create_random_key_selector(s->cct);
+
+	  set_attr(attrs, RGW_ATTR_CRYPT_KEYSEL, key_selector);
+	  set_attr(attrs, RGW_ATTR_CRYPT_CONTEXT, cooked_context);
+	  set_attr(attrs, RGW_ATTR_CRYPT_MODE, "AES256");
+	  set_attr(attrs, RGW_ATTR_CRYPT_KEYID, key_id);
+	  std::string actual_key;
+	  res = make_actual_key_from_sse_s3(s->cct, attrs, actual_key);
+          if (res != 0) {
+	    ldpp_dout(s, 5) << "ERROR: failed to retrieve actual key from key_id: " << key_id << dendl;
+	    s->err.message = "Failed to retrieve the actual key " ;
+	    return res;
+	  }
+	  if (actual_key.size() != AES_256_KEYSIZE) {
+	    ldpp_dout(s, 5) << "ERROR: key obtained from key_id:" <<
+                           key_id << " is not 256 bit size" << dendl;
+	    s->err.message = "KMS provided an invalid key for the given kms-keyid.";
+	    return -ERR_INVALID_ACCESS_KEY;
+	  }
+
+          if (block_crypt) {
+	    auto aes = std::unique_ptr<AES_256_CBC>(new AES_256_CBC(s->cct));
+	    aes->set_key(reinterpret_cast<const uint8_t*>(actual_key.c_str()), AES_256_KEYSIZE);
+	    *block_crypt = std::move(aes);
+	  }
+          ::ceph::crypto::zeroize_for_security(actual_key.data(), actual_key.length());
+
+	  crypt_http_responses["x-amz-server-side-encryption"] = "AES256";
+
+	  return 0;
+	} else {
+          ldpp_dout(s, 5) << "ERROR: SSE algorithm: " 
+		  <<  bucket_encryption_conf.sse_algorithm() 
+		  << " not supported" << dendl;
+	  s->err.message = "The requested encryption algorithm is not valid, must be AES256.";
+	  return -ERR_INVALID_ENCRYPTION_ALGORITHM;
+	}
+      } catch (const buffer::error& e) {
+        ldpp_dout(s, 5) << __func__ <<  "decode bucket_encryption_conf failed" << dendl;
       }
     }
 
